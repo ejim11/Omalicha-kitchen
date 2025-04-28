f04f9973f71ae072560d8941265a24c06dc24d06
ejim11 <favourejim56@gmail.com>
2025-04-28 18:14:17 +0100

    ✅ home comp

---
+++ components/HomeComp/HomeComp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./Hero";
+
+const HomeComp = () => {
+  return (
+    <main className="-mt-[10rem]">
+      <Hero />
+    </main>
+  );
+};
+
+export default HomeComp;
