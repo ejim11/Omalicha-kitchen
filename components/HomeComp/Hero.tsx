5f9963f5751d19144b71b226b1de8fb62938ffbd
ejim11 <favourejim56@gmail.com>
2025-04-28 18:14:06 +0100

    ✅ hero comp

---
+++ components/HomeComp/Hero.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="bg-hero h-screen flex justify-center px-[9rem]  flex-col">
+      <div className="w-[75%]">
+        <h2 className=" text-white font-ojuju  leading-[8.2rem] font-bold text-[9.6rem]">
+          Taste of Home on your Plate
+        </h2>
+        <p className="font-switzer text-[2rem] leading-[3.3rem] text-white w-[55%] mt-[1.6rem]">
+          African restaurant in Houston, Texas for rich, authentic dishes that
+          bring family, culture, and comfort together.
+        </p>
+        <Link
+          href={"/menu"}
+          className="bg-secondary-1 px-[3rem] py-[1rem] rounded-[1.6rem] mt-[2.4rem] font-switzer font-medium block w-max text-[2rem]"
+        >
+          Explore our dishes
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
