e00610d2c7fab8aaf890ff82b5572f32d5981cc7
ejim11 <favourejim56@gmail.com>
2025-04-30 13:20:14 +0100

    🗃️ added the special dishes data

---
+++ data/specialDishes.ts
@@ -0,0 +1,36 @@
+import { FoodItem } from "@/types/foodItem";
+import riceDishesImg from "../assets/home/rice-dishes.png";
+import pepperSoupImg from "../assets/home/pepper-soup.png";
+import traditionalSoupImg from "../assets/home/traditional-soup.png";
+import omaSpecialImg from "../assets/home/oma-special.png";
+
+export const specialDishes: FoodItem[] = [
+  {
+    name: "rice dishes",
+    description:
+      "Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor",
+    price: 12,
+    image: riceDishesImg,
+  },
+  {
+    name: "pepper soup",
+    description:
+      "Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor",
+    price: 12,
+    image: pepperSoupImg,
+  },
+  {
+    name: "traditional soup",
+    description:
+      "Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor",
+    price: 12,
+    image: traditionalSoupImg,
+  },
+  {
+    name: "oma special",
+    description:
+      "Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor",
+    price: 12,
+    image: omaSpecialImg,
+  },
+];
