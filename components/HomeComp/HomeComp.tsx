cbf333485b404d9f3572180fabeeadf37671b45f
ejim11 <favourejim56@gmail.com>
2025-04-30 13:12:19 +0100

    ✅ added the special dishes comp

--- components/HomeComp/HomeComp.tsx
+++ components/HomeComp/HomeComp.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
+import SpecialDishes from "./SpecialDishes";
 
 const HomeComp = () => {
   return (
     <main className="-mt-[10rem]">
       <Hero />
+      <div className="bg-[#F4F3E8] px-[9rem] py-[3rem]">
+        <SpecialDishes />
+      </div>
     </main>
   );
 };
