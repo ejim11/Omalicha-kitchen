87befdf7edce14b3d631c2a582dbffa9c155e8f4
ejim11 <favourejim56@gmail.com>
2025-05-01 11:54:18 +0100

    🗃️ added testimonials data

---
+++ data/testimonials.ts
@@ -0,0 +1,23 @@
+import { TestimonialType } from "@/types/testimonial";
+import customerImg from "../assets/home/testimonial-1.png";
+
+export const testimonials: TestimonialType[] = [
+  {
+    customerName: "Ama Ampomah",
+    customerImage: customerImg,
+    text: "Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu aliquam quis. Ornare tincidunt tempus semper",
+    stars: 3,
+  },
+  {
+    customerName: "Joy Ikpeme",
+    customerImage: customerImg,
+    text: "Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu aliquam quis. Ornare tincidunt tempus semper",
+    stars: 5,
+  },
+  {
+    customerName: "Juliet Mimi",
+    customerImage: customerImg,
+    text: "Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu aliquam quis. Ornare tincidunt tempus semper",
+    stars: 4,
+  },
+];
