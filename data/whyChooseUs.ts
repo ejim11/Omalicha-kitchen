1cdf3bd97c8e7cf319b868b7343a2e98d294476b
ejim11 <favourejim56@gmail.com>
2025-05-02 10:33:53 +0100

    🗃️ added the data for the why choose us section

---
+++ data/whyChooseUs.ts
@@ -0,0 +1,28 @@
+import { WhyChooseUsType } from "@/types/whyChooseUs";
+import whyChooseUsImg1 from "../assets/home/why-choose-us-1.png";
+import whyChooseUsImg2 from "../assets/home/why-choose-us-2.png";
+import whyChooseUsImg3 from "../assets/home/why-choose-us-3.png";
+import whyChooseUsImg4 from "../assets/home/why-choose-us-4.png";
+
+export const whyChooseUsData: WhyChooseUsType[] = [
+  {
+    image: whyChooseUsImg1,
+    title: "Authentic Taste",
+    text: "Rich Traditional African Dishes and recipes that are delicious and flavorful.",
+  },
+  {
+    image: whyChooseUsImg2,
+    title: "Quality",
+    text: "Fresh and high-quality ingredients in every dish.",
+  },
+  {
+    image: whyChooseUsImg3,
+    title: "Fast Delivery",
+    text: "Get your favorite meals fresh, hot, delivered right on time!",
+  },
+  {
+    image: whyChooseUsImg4,
+    title: "Community",
+    text: "A warm and beautiful space where our African Family and Food lovers bond.",
+  },
+];
