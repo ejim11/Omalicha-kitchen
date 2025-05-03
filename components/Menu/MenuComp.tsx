c26b71452b087d0ef008e88ccda77aab3bca8fd1
ejim11 <favourejim56@gmail.com>
2025-05-03 14:45:28 +0100

    🎨 improved code structure

--- components/Menu/MenuComp.tsx
+++ components/Menu/MenuComp.tsx
@@ -3,6 +3,7 @@ import { menu, MenuItem } from "@/data/menu";
 import { useSearchParams } from "next/navigation";
 import React, { useMemo } from "react";
 import DishesList from "./DishesList";
+import { DishItem } from "@/types/dishItem";
 
 const MenuComp = () => {
   const searchParams = useSearchParams();
@@ -17,22 +18,24 @@ const MenuComp = () => {
 
   const menuItem: MenuItem[] = menu.filter(
     (item: MenuItem) =>
-      item.title.toLowerCase().split(" ").join("-") ===
+      item.filter.toLowerCase().split(" ").join("-") ===
       searchParamsObject.filter
   );
 
   return (
     <section className="mt-[4rem] relative z-50">
       <div>
-        {menuItem.map((item: MenuItem) => (
-          <div key={item.title} className="font-switzer">
-            <p className="font-medium text-[2.4rem] text-primary-1 mb-[0.8rem]">
-              {item.title}
-            </p>
-            <p className="text-[1.8rem] text-[#797B78]">{item.text}</p>
-            <DishesList dishItems={item.dishItems} />
-          </div>
-        ))}
+        {menuItem[0].items.map(
+          (item: { title: string; text: string; dishItems: DishItem[] }) => (
+            <div key={item.title} className="font-switzer mb-[4rem] last:mb-0">
+              <p className="font-medium text-[2.4rem] text-primary-1 mb-[0.8rem]">
+                {item.title}
+              </p>
+              <p className="text-[1.8rem] text-[#797B78]">{item.text}</p>
+              <DishesList dishItems={item.dishItems} />
+            </div>
+          )
+        )}
       </div>
     </section>
   );
