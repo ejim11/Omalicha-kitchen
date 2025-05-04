e1795fef68919a1378c6b6e4a435fe22cebe5312
ejim11 <favourejim56@gmail.com>
2025-05-04 15:39:24 +0100

    ✅ added the about us comp

---
+++ components/AboutUs/AboutUsComp.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ContainerWithFixedImages from "../ContainerWithFixedImages";
+import MainContainer from "../MainContainer";
+import Testimonial from "../HomeComp/Testimonial";
+import { AboutUsData, aboutUsData } from "@/data/aboutUsData";
+import AboutUsItem from "./AboutUsItem";
+
+const AboutUsComp = () => {
+  return (
+    <MainContainer>
+      <ContainerWithFixedImages
+        title="About Us"
+        text="Welcome to Omalicha Kitchen, your go-to destination for delicious, home-cooked meals prepared with love, passion, and the highest hygiene standards. We take pride in serving fresh, flavorful, and nutritious dishes in a clean and well-maintained environment."
+      >
+        <div className="w-full flex flex-col ">
+          <div className="flex flex-col">
+            {aboutUsData.map((data: AboutUsData, i: number) => (
+              <AboutUsItem
+                key={data.title}
+                index={i}
+                title={data.title}
+                text={data.text}
+              />
+            ))}
+          </div>
+          <div className="w-full flex flex-col items-center text-center mt-[3rem] font-switzer">
+            <h3 className="font-medium text-[3.2rem] text-primary-1">
+              Order now
+            </h3>
+            <p className="text-[1.4rem] text-black">
+              Order Now and join our family of satisfied customers who enjoy
+              tasty and flavorful African dishes
+            </p>
+          </div>
+        </div>
+      </ContainerWithFixedImages>
+      <div className="w-full pb-[4rem]">
+        <Testimonial />
+      </div>
+    </MainContainer>
+  );
+};
+
+export default AboutUsComp;
