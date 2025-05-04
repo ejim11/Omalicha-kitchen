adddc53f21d44fb6788bca95297cd397c25474e9
ejim11 <favourejim56@gmail.com>
2025-05-04 15:40:51 +0100

    🗃️ added about us data

---
+++ data/aboutUsData.ts
@@ -0,0 +1,15 @@
+export type AboutUsData = {
+  title: string;
+  text: string;
+};
+
+export const aboutUsData: AboutUsData[] = [
+  {
+    title: "Our Mission",
+    text: "Omalicha Kitchen is a family-owned African restaurant in Houston, celebrating the rich, authentic flavors of West African cuisine in every dish.",
+  },
+  {
+    title: "Our Story",
+    text: "We serve a variety of mouthwatering dishes, from traditional African delicacies to contemporary favorites. Whether you're craving a hearty local meal, a quick bite, or something indulgent, our menu has something for everyone.",
+  },
+];
