1af866f5cb8f2b642846e5eb5c3382e0491e7a1f
ejim11 <favourejim56@gmail.com>
2025-05-04 15:40:28 +0100

    🎨 formatted the menu nav and created reusable comp

--- components/Menu/MenuNav.tsx
+++ components/Menu/MenuNav.tsx
@@ -1,11 +1,10 @@
 "use client";
 import React, { ReactNode, useMemo } from "react";
 import StockUpForTheWeek from "../HomeComp/StockUpForTheWeek";
-import flower1Img from "../../assets/menu/flower-1.png";
-import flower2Img from "../../assets/menu/flower-2.png";
-import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import ContainerWithFixedImages from "../ContainerWithFixedImages";
+import MainContainer from "../MainContainer";
 
 const MenuNav: React.FC<{ children: ReactNode }> = ({ children }) => {
   const searchParams = useSearchParams();
@@ -29,47 +28,11 @@ const MenuNav: React.FC<{ children: ReactNode }> = ({ children }) => {
   ];
 
   return (
-    <main className="px-[9rem] pt-[4rem]  overflow-x-hidden">
-      <div className="relative ">
-        <div className="w-[13.6rem] absolute  left-0 z-10">
-          <Image
-            src={flower1Img}
-            alt="flower image"
-            priority
-            width={200}
-            height={200}
-            className="w-full h-full"
-          />
-        </div>
-        <div className="w-[21rem] absolute  -right-[7rem] -top-[1rem] z-10">
-          <Image
-            src={flower1Img}
-            alt="flower image"
-            priority
-            width={200}
-            height={200}
-            className="w-full h-full"
-          />
-        </div>
-        <div className="w-[13.6rem] absolute  -right-[9rem] bottom-0 z-10">
-          <Image
-            src={flower2Img}
-            alt="flower image"
-            priority
-            width={200}
-            height={200}
-            className="w-full h-full"
-          />
-        </div>
-        <div className="flex items-center flex-col text-center w-full z-40 relative">
-          <h3 className="text-[3.2rem] font-poppins font-medium capitalize text-primary-1">
-            our menu
-          </h3>
-          <p className="leading-[2.4rem] text-[#181818] font-switzer w-[60%]">
-            Explore the rich flavors of West Africa-from fresh rice dishes to
-            traditional soups and authentic Igbo Delicacies
-          </p>
-        </div>
+    <MainContainer>
+      <ContainerWithFixedImages
+        title="our menu"
+        text="Explore the rich flavors of West Africa-from fresh rice dishes to traditional soups and authentic Igbo Delicacies"
+      >
         <div className="flex z-50 mt-[4rem] justify-center relative text-[1.8rem] font-switzer">
           {navs.map((nav: string) => (
             <Link
@@ -87,9 +50,9 @@ const MenuNav: React.FC<{ children: ReactNode }> = ({ children }) => {
           ))}
         </div>
         <div>{children}</div>
-      </div>
+      </ContainerWithFixedImages>
       <StockUpForTheWeek />
-    </main>
+    </MainContainer>
   );
 };
 
