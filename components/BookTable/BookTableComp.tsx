bc5f9d2a22f860683715d08e19d5cc54935bfe23
ejim11 <favourejim56@gmail.com>
2025-05-05 00:04:55 +0100

    ✅ added the book table comp

---
+++ components/BookTable/BookTableComp.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useState } from "react";
+import MainContainer from "../MainContainer";
+import ContainerWithFixedImages from "../ContainerWithFixedImages";
+import BookTableForm from "./BookTableForm";
+import Reservation from "./Reservation";
+
+const BookTableComp = () => {
+  const [reservationInfo, setReservationInfo] = useState();
+
+  return (
+    <MainContainer>
+      {reservationInfo ? (
+        <Reservation reservation={reservationInfo} />
+      ) : (
+        <ContainerWithFixedImages
+          title="Book your Table"
+          text="Any  question or remark ? Just write us a message"
+        >
+          <div className="border border-[#D1D1D1] px-[9rem] py-[5rem] bg-[#F4F3E8] relative z-50 mt-[7.3rem] mb-[5rem]">
+            <BookTableForm setReservationInfo={setReservationInfo} />
+          </div>
+        </ContainerWithFixedImages>
+      )}
+    </MainContainer>
+  );
+};
+
+export default BookTableComp;
