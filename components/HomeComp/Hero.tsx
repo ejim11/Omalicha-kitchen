1e43534884c9e68e3b8bbbb65fbca695c6c716b0
ejim11 <favourejim56@gmail.com>
2025-05-05 18:24:09 +0100

    🐛 fixed bg image bug

--- components/HomeComp/Hero.tsx
+++ components/HomeComp/Hero.tsx
@@ -4,21 +4,23 @@ import FirstSection from "../FirstSection";
 
 const Hero = () => {
   return (
-    <FirstSection containerClassName="bg-hero h-screen flex justify-center max-ssm:py-[8rem] max-ssm:min-h-screen px-[9rem] max-2xl:px-[5rem] max-xl:px-[3rem] max-sm:px-[2rem]  flex-col">
-      <div className="w-[75%] max-xlg:w-full">
-        <h2 className=" text-white font-ojuju  leading-[8.2rem] max-sm:text-[7rem] max-sm:mb-[1rem] font-bold text-[9.6rem]">
-          Taste of Home on your Plate
-        </h2>
-        <p className="font-switzer text-[2rem] leading-[3.3rem] text-white w-[55%] max-sm:w-full mt-[1.6rem]">
-          African restaurant in Houston, Texas for rich, authentic dishes that
-          bring family, culture, and comfort together.
-        </p>
-        <Link
-          href={"/menu?filter=traditional-soups"}
-          className="bg-secondary-1 px-[3rem] py-[1rem] rounded-[1.6rem] mt-[2.4rem] font-switzer font-medium block w-max text-[2rem] text-black"
-        >
-          Explore our dishes
-        </Link>
+    <FirstSection containerClassName="bg-hero  h-screen ">
+      <div className="flex justify-center w-full bg-[rgba(48,66,52,0.7)] h-full max-ssm:py-[8rem] max-ssm:min-h-screen px-[9rem] max-2xl:px-[5rem] max-xl:px-[3rem] max-sm:px-[2rem]  flex-col">
+        <div className="w-[75%] max-xlg:w-full ">
+          <h2 className=" text-white font-ojuju  leading-[8.2rem] max-sm:text-[7rem] max-sm:mb-[1rem] font-bold text-[9.6rem]">
+            Taste of Home on your Plate
+          </h2>
+          <p className="font-switzer text-[2rem] leading-[3.3rem] text-white w-[55%] max-sm:w-full mt-[1.6rem]">
+            African restaurant in Houston, Texas for rich, authentic dishes that
+            bring family, culture, and comfort together.
+          </p>
+          <Link
+            href={"/menu?filter=traditional-soups"}
+            className="bg-secondary-1 px-[3rem] py-[1rem] rounded-[1.6rem] mt-[2.4rem] font-switzer font-medium block w-max text-[2rem] text-black"
+          >
+            Explore our dishes
+          </Link>
+        </div>
       </div>
     </FirstSection>
   );
