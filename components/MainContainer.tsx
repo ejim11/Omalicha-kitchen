58ca41b151f8ff1ca6f08bca2f966dc86d312ef4
ejim11 <favourejim56@gmail.com>
2025-05-05 12:50:59 +0100

    💄 changed the top padding based on is header sticky

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,8 +1,18 @@
-import React, { FC, ReactNode } from "react";
+"use client";
+import appContext from "@/store/appContext";
+import React, { FC, ReactNode, useContext } from "react";
 
 const MainContainer: FC<{ children: ReactNode }> = ({ children }) => {
+  const { isHeaderSticky } = useContext(appContext);
+
   return (
-    <main className="px-[9rem] pt-[4rem]  overflow-x-hidden">{children}</main>
+    <main
+      className={`px-[9rem] ${
+        isHeaderSticky ? "pt-[8rem]" : "pt-[4rem]"
+      }   overflow-x-hidden`}
+    >
+      {children}
+    </main>
   );
 };
 
