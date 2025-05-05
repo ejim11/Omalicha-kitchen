580f543462bee8d7ac91ad083a44f3a0a34e465a
ejim11 <favourejim56@gmail.com>
2025-05-05 12:48:51 +0100

    ✅ added search functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,24 @@
 "use client";
-import React from "react";
+import React, { ChangeEvent, useContext, useEffect, useState } from "react";
 import logoImg from "../assets/logo.svg";
 import Image from "next/image";
 import { NavLink } from "@/types/navLink";
 import Link from "next/link";
 import { GoSearch } from "react-icons/go";
 import { FiShoppingCart } from "react-icons/fi";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import appContext from "@/store/appContext";
 
 const Header = () => {
   const pathname = usePathname();
+
+  const router = useRouter();
+
+  const { cartItems, isHeaderSticky } = useContext(appContext);
+
+  const [scaleUp, setScaleUp] = useState<boolean>(false);
+
+  const [searchText, setSearchText] = useState<string>("");
 
   const navLinks: NavLink[] = [
     {
@@ -30,8 +39,48 @@ const Header = () => {
     },
   ];
 
+  const searchTextInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    setScaleUp(true);
+    const timer = setTimeout(() => {
+      setScaleUp(false);
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+      setScaleUp(false);
+    };
+  }, [cartItems]);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let timer: any;
+
+    if (searchText) {
+      timer = setTimeout(() => {
+        const search = searchText.slice().toLowerCase().split(" ").join("-");
+        router.replace(`/menu?filter=all&dish=${search}`);
+      }, 1000);
+    } else {
+      router.replace(`/menu?filter=all&dish=${""}`);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [router, searchText]);
+
   return (
-    <header className="h-[10rem] flex items-center px-[9rem]   w-full  bg-[rgba(255,255,255,0.66)] justify-between relative z-[50]">
+    <header
+      className={`h-[10rem] flex items-center px-[9rem]   w-full   justify-between z-[100]  ${
+        isHeaderSticky
+          ? `fixed top-0  bg-white  showHeader`
+          : "relative bg-[rgba(255,255,255,0.66)] "
+      }`}
+    >
       <Link href={"/"} className="w-[10rem]">
         <Image
           src={logoImg}
@@ -61,19 +110,28 @@ const Header = () => {
       <div className="flex items-center">
         <div className="w-[14.6rem] border rounded-[2.6rem] border-primary-1 flex items-center pl-[1rem]">
           <GoSearch className="w-[1.8rem] h-[1.8rem] text-primary-1" />
-          <div className="flex-1">
+          <div className="flex-1 overflow-hidden">
             <input
               type="text"
               name="search"
               id="search"
-              className="w-full  p-[1rem] placeholder:text-[1.2rem] placeholder:text-[rgba(0,0,0,0.33)] placeholder:font-switzer ring-0 outline-hidden focus:ring-0 focus:outline-hidden"
+              value={searchText}
+              onChange={searchTextInputChangeHandler}
+              className="w-full  p-[1rem] placeholder:text-[1.2rem] placeholder:text-[rgba(0,0,0,0.33)] placeholder:font-switzer ring-0 outline-hidden focus:ring-0 focus:outline-hidden focus:bg-transparent"
               placeholder="Search here"
             />
           </div>
         </div>
-        <button type="button" className="mx-[5rem]">
+        <Link href={"/your-order"} className="mx-[5rem] block relative">
           <FiShoppingCart className="w-[2.4rem] h-[2.4rem] text-black" />
-        </button>
+          <span
+            className={`${cartItems.length > 0 ? "flex " : "hidden"} ${
+              scaleUp ? "scale-125" : "scale-100"
+            } text-white -top-[1rem] -right-[0.8rem] bg-red-600 w-[2rem] h-[2rem] text-[1.2rem] rounded-full absolute items-center justify-center transition-all ease-in duration-200`}
+          >
+            {cartItems.length}
+          </span>
+        </Link>
         <Link href={"/book-table"} className="font-switzer text-[1.8rem]">
           Order Online/Reservation
         </Link>
