3a6560f1ef4b2eaa88ca0ff890dc661afb575e0e
ejim11 <favourejim56@gmail.com>
2025-05-10 12:34:27 +0100

    🗃️ updated the testimonial data

--- data/testimonials.ts
+++ data/testimonials.ts
@@ -3,21 +3,24 @@ import customerImg from "../assets/home/testimonial-1.png";
 
 export const testimonials: TestimonialType[] = [
   {
-    customerName: "Ama Ampomah",
+    customerName: "Adesewa Ekhigoho",
     customerImage: customerImg,
-    text: "Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu aliquam quis. Ornare tincidunt tempus semper",
-    stars: 3,
-  },
-  {
-    customerName: "Joy Ikpeme",
-    customerImage: customerImg,
-    text: "Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu aliquam quis. Ornare tincidunt tempus semper",
-    stars: 5,
-  },
-  {
-    customerName: "Juliet Mimi",
-    customerImage: customerImg,
-    text: "Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu aliquam quis. Ornare tincidunt tempus semper",
+    text: "The Pepper Soup here is the real deal. It's spicy, flavorful, and just how a Pepper soup should taste. Came twice this week already.",
     stars: 4,
+    address: "Houston, TX  Downtown ",
+  },
+  {
+    customerName: "Chigioke Nelson",
+    customerImage: customerImg,
+    text: "Ordered the Oma Special for the first time, and wow. It's so rich, and the meat was well cooked sand tender. Definitely coming back.",
+    stars: 5,
+    address: "Houston, TX Brays Oaks ",
+  },
+  {
+    customerName: "Ifeoma Ndukwe",
+    customerImage: customerImg,
+    text: "Their Fried Rice is my go-to comfort food. Perfectly seasoned, not too oily, it’s worth the cost.",
+    stars: 4,
+    address: "Houston, Meyerland",
   },
 ];
