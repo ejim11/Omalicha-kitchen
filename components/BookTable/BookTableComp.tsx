70dacb1736de7c22c2eb193599190bebde735fa9
ejim11 <favourejim56@gmail.com>
2025-05-10 12:29:25 +0100

    ✅ changed text

--- components/BookTable/BookTableComp.tsx
+++ components/BookTable/BookTableComp.tsx
@@ -15,7 +15,7 @@ const BookTableComp = () => {
       ) : (
         <ContainerWithFixedImages
           title="Book your Table"
-          text="Any  question or remark ? Just write us a message"
+          text="Order for a meal and get it delivered to you. Or book a reservation at our restaurant."
         >
           <div className="border border-[#D1D1D1] px-[9rem] max-2xl:px-[7rem] max-xl:px-[5rem] max-lg:p-[3rem] max-ssm:p-[1.5rem] py-[5rem] bg-[#F4F3E8] relative z-50 mt-[7.3rem] mb-[5rem]">
             <BookTableForm setReservationInfo={setReservationInfo} />
