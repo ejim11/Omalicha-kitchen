2862054af15a9aaf14f26231c62e28d50f26d4ca
ejim11 <favourejim56@gmail.com>
2025-10-01 02:27:59 +0100

    ✅ updated the place order button

--- components/HomeComp/PlaceYourOrder.tsx
+++ components/HomeComp/PlaceYourOrder.tsx
@@ -1,7 +1,15 @@
-import Link from "next/link";
+"use client";
 import React from "react";
+import { useContext } from "react";
+import appContext from "@/store/appContext";
 
 const PlaceYourOrder = () => {
+  const { changeIsOrderModalVisible } = useContext(appContext);
+
+  const openModalHandler = () => {
+    changeIsOrderModalVisible(true);
+  };
+
   return (
     <section className="h-[64rem] w-full mt-[8rem] bg-place-order rounded-[1.6rem] overflow-hidden px-[7rem] max-2xl:px-[5rem] max-xl:px-[3rem] max-smd:p-[2rem] max-ssm:p-[1rem] flex flex-col justify-center ">
       <div className="text-white w-[50%] max-xl:w-[75%] max-xmd:w-full max-smd:flex max-smd:flex-col max-smd:text-center max-smd:items-center ">
@@ -13,12 +21,12 @@ const PlaceYourOrder = () => {
           Explore our tasty jollof rice, egusi soup, and all your favorite
           mouth-wavering African dishes prepared with love, care, and pride.
         </p>
-        <Link
-          href={"/menu"}
-          className="bg-secondary-1 px-[3rem] py-[1rem] rounded-[1.6rem] mt-[2.4rem] font-switzer font-medium block w-max text-[2rem] text-black"
+        <button
+          className="bg-secondary-1 px-[3rem] py-[1rem] rounded-[1.6rem] mt-[2.4rem] font-switzer font-medium block w-max text-[2rem] text-black cursor-pointer"
+          onClick={openModalHandler}
         >
           Place your order
-        </Link>
+        </button>
       </div>
     </section>
   );
